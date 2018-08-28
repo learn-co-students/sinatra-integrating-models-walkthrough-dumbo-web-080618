17a1caba0b7a1b0e96a2d5f458db807de2394a80
github username <github email address>
2018-08-28 17:06:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,9 +5,11 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  #let's take the data from params[user_text] (in the post '/' do route) and feed it into a new instance of theTextAnalyzer class:
+  #We now have the instance of TextAnalyzer saved to an instance variable called @analyzed_text. This means that we can call it and its methods from the results.erb view that is being rendered, using erb tags!
   post '/' do
     text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
