4a45a789581c9b17c3f536060f31afc438508614
github username <github email address>
2018-08-28 10:16:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,9 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+    @most_common_occurs = @analyzed_text.most_used_letter.values.first
+    @most_common_letter = @analyzed_text.most_used_letter.key(@most_common_occurs)
     erb :results
   end
 end
